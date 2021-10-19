3751f13d7fa7b0a61318f4e38a19199cf22298c3
Sawyer Cherry <sawyeracherry@gmail.com>
2021-10-19 13:57:43 -0500

    fixed buttons added colors

--- Fluently/View/Sub Views/Buttons/StartButtonView.swift
+++ Fluently/View/Sub Views/Buttons/StartButtonView.swift
@@ -18,14 +18,14 @@ struct StartButtonView: View {
         }) {
             HStack(spacing: 8) {
                 Text("Start")
-                    .font(.largeTitle)
+                    .font(.title)
                 Image(systemName: "arrow.right.circle")
                     .imageScale(.large)
             }
             .padding(.horizontal, 16)
             .padding(.vertical, 10)
             .background(
-                Capsule().strokeBorder(Color.black, lineWidth: 1.75))
+                Capsule().strokeBorder(Color.black, lineWidth: 1.25))
         }//: BUTTON
         .accentColor(Color.black)
     }
